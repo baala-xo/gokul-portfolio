0733f30a303a19eb43f1f59f3b0eff5e5def4c29
bala <bala2003fd@gmail.com>
2025-04-23 22:05:52 +0530

    updated desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Gokul portfolio',
+  description: 'UI UX Portfolio @bala.abq',
+  generator: 'me ofc',
 }
 
 export default function RootLayout({
